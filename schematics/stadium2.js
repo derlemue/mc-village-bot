1e423e2def88eec4bf5074273507ab6d5574bd0a
derlemue <thomas.ledermueller@icloud.com>
2025-11-30 06:23:19 +0100

    version 2.0.3-alpha

---
+++ schematics/stadium2.js
@@ -0,0 +1,70 @@
+module.exports = {
+
+name: 'stadium2',
+
+width: 82, height: 27, depth: 66,
+
+foundation: 'stone', foundationHeight: 1,
+
+walls: 'red_stained_glass_pane',
+
+roof: 'red_sandstone_stairs',
+
+doorPos: { x: 41, z: 0 },
+
+details: [
+
+  { x: 39, y: 0, z: 1, block: 'acacia_door' },
+  { x: 42, y: 0, z: 1, block: 'acacia_door' },
+  { x: 36, y: 5, z: 1, block: 'acacia_door' },
+  { x: 45, y: 5, z: 1, block: 'acacia_door' },
+  { x: 37, y: 17, z: 2, block: 'dark_oak_sign' },
+  { x: 44, y: 17, z: 2, block: 'dark_oak_sign' },
+  { x: 39, y: 1, z: 3, block: 'lantern' },
+  { x: 42, y: 1, z: 3, block: 'lantern' },
+  { x: 39, y: 12, z: 3, block: 'acacia_door' },
+  { x: 42, y: 12, z: 3, block: 'acacia_door' },
+  { x: 19, y: 17, z: 3, block: 'acacia_door' },
+  { x: 36, y: 17, z: 3, block: 'acacia_door' },
+  { x: 45, y: 17, z: 3, block: 'acacia_door' },
+  { x: 62, y: 17, z: 3, block: 'acacia_door' },
+  { x: 19, y: 18, z: 3, block: 'acacia_door' },
+  { x: 38, y: 18, z: 3, block: 'birch_wall_sign' },
+  { x: 43, y: 18, z: 3, block: 'birch_wall_sign' },
+  { x: 62, y: 18, z: 3, block: 'acacia_door' },
+  { x: 26, y: 20, z: 3, block: 'birch_wall_sign' },
+  { x: 30, y: 20, z: 3, block: 'birch_wall_sign' },
+  { x: 51, y: 20, z: 3, block: 'birch_wall_sign' },
+  { x: 55, y: 20, z: 3, block: 'birch_wall_sign' },
+  { x: 27, y: 21, z: 3, block: 'birch_wall_sign' },
+  { x: 29, y: 21, z: 3, block: 'birch_wall_sign' },
+  { x: 52, y: 21, z: 3, block: 'birch_wall_sign' },
+  { x: 54, y: 21, z: 3, block: 'birch_wall_sign' },
+  { x: 34, y: 10, z: 5, block: 'dark_oak_wall_sign' },
+  { x: 47, y: 10, z: 5, block: 'dark_oak_wall_sign' },
+  { x: 37, y: 23, z: 5, block: 'lantern' },
+  { x: 44, y: 23, z: 5, block: 'lantern' },
+  { x: 40, y: 25, z: 5, block: 'dark_oak_hanging_sign' },
+  { x: 41, y: 25, z: 5, block: 'dark_oak_hanging_sign' },
+  { x: 22, y: 0, z: 6, block: 'lantern' },
+  { x: 39, y: 0, z: 6, block: 'lantern' },
+  { x: 42, y: 0, z: 6, block: 'lantern' },
+  { x: 59, y: 0, z: 6, block: 'lantern' },
+  { x: 26, y: 1, z: 6, block: 'lantern' },
+  { x: 30, y: 1, z: 6, block: 'lantern' },
+  { x: 51, y: 1, z: 6, block: 'lantern' },
+  { x: 55, y: 1, z: 6, block: 'lantern' },
+  { x: 19, y: 2, z: 6, block: 'acacia_door' },
+  { x: 62, y: 2, z: 6, block: 'acacia_door' },
+  { x: 19, y: 3, z: 6, block: 'acacia_door' },
+  { x: 62, y: 3, z: 6, block: 'acacia_door' },
+  { x: 22, y: 5, z: 6, block: 'lantern' },
+  { x: 59, y: 5, z: 6, block: 'lantern' },
+  { x: 26, y: 6, z: 6, block: 'lantern' },
+  { x: 30, y: 6, z: 6, block: 'lantern' },
+  { x: 51, y: 6, z: 6, block: 'lantern' },
+  { x: 55, y: 6, z: 6, block: 'lantern' }
+
+]
+
+};
---
+++ schematics/tree.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+name: 'tree',
+
+width: 7, height: 11, depth: 7,
+
+foundation: 'stone', foundationHeight: 1,
+
+walls: 'red_sandstone',
+
+roof: 'red_sandstone_stairs',
+
+doorPos: { x: 3, z: 0 },
+
+details: []
+
+};
