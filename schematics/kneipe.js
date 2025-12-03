b5139934be0ffd1ba7c7e05da5cd164ecc2ad43b
derlemue <thomas.ledermueller@icloud.com>
2025-12-03 18:48:20 +0100

    Release v2.1.9-alpha

--- schematics/kneipe.js
+++ schematics/kneipe.js
@@ -4,10 +4,10 @@ module.exports = {
   foundation: 'stone_bricks', foundationHeight: 1,
   walls: 'spruce_wood',
   roof: 'spruce_stairs',
-  doorPos: { x: 8, z: 0 },
+  doorPos: { x: 4, z: 0 },
   details: [
-    { x: 7, y: 1, z: 0, block: 'spruce_door' },
-    { x: 7, y: 2, z: 0, block: 'spruce_door' },
+    { x: 4, y: 1, z: 0, block: 'spruce_door' },
+    { x: 4, y: 2, z: 0, block: 'spruce_door' },
     { x: 1, y: 2, z: 1, block: 'lantern' },
     { x: 14, y: 2, z: 14, block: 'lantern' },
     { x: 8, y: 4, z: 8, block: 'brewing_stand' }
