61b0001729d042529949f4df4c05fbbc6979a401
derlemue <thomas.ledermueller@icloud.com>
2025-12-04 23:16:55 +0100

    Release v2.2.7-alpha

---
+++ schematics/freiraum.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Freiraum',
+  width: 11, height: 1, depth: 11,
+  foundation: 'stone_bricks', foundationHeight: 1,
+  walls: 'air',
+  roof: 'air',
+  doorPos: { x: 6, z: 0 },
+  details: [
+    { x: 6, y: 1, z: 6, block: 'lantern' }
+  ]
+};
